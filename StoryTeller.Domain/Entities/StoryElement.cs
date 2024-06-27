0dcc773db9bd5c9101f36d310bd519bacad8f4a6
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-06-27 14:17:17 +0300

    Add project files.

---
+++ StoryTeller.Domain/Entities/StoryElement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryTeller.Domain.Entities
+{
+    public class StoryElement
+    {
+        public int Id { get; set; }
+        public StoryElementType Type { get; set; } // Enum kullanarak tipi belirliyoruz
+        public string Description { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ StoryTeller.Domain/Enums/StoryElementType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryTeller.Domain.Enums
+{
+    public enum StoryElementType
+    {
+        Character,
+        Location,
+        Event
+    }
+}
