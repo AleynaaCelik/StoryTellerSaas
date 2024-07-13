30e40bf37b0f1faa50eab05e291e0385929e51cd
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-07-13 23:07:57 +0300

    UserAuthValidatorBase Added

---
+++ StoryTeller.Application/Common/FluentValidation/UserAuthValidatorBase.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using MextFullStackSaas.Application.Common.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace MextFullStackSaas.Application.Common.FluentValidation.BaseValidators
+{
+    public class UserAuthValidatorBase<T> : AbstractValidator<T> where T : class
+    {
+        protected readonly IIdentityService _identityService;
+
+        public UserAuthValidatorBase(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        protected bool IsEmail(string email)
+        {
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, emailPattern, RegexOptions.IgnoreCase);
+        }
+
+        protected bool IsValidTitle(string title)
+        {
+            return !string.IsNullOrWhiteSpace(title) && title.Length <= 100;
+        }
+
+        protected bool IsValidList(List<string> list)
+        {
+            return list != null && list.Any() && list.All(item => !string.IsNullOrWhiteSpace(item));
+        }
+
+        protected bool IsValidGenre(string genre)
+        {
+            return !string.IsNullOrWhiteSpace(genre);
+        }
+
+        protected bool IsValidLength(int length)
+        {
+            return length > 0;
+        }
+    }
+}
